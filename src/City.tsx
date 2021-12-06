108066f5acf3a271aea1aa8d30eddb64fa36e828
justinWaves <juhstinn@gmail.com>
2021-12-06 15:08:26 -0800

    added theme togglefunctions to header and city

--- src/City.tsx
+++ src/City.tsx
@@ -1,14 +1,23 @@
-import React from "react";
 import "./City.css";
 import BridgeForeground from "./images/ggb-foreground.svg";
 import BridgeBackground from "./images/ggb-background.svg";
 import Clouds from "./images/clouds.svg";
 import SunMoon from "./images/sunandmoon.svg";
+import { useTheme } from "./ThemeManager";
 
 function City() {
+  const theme = useTheme();
+  const toggleSky = (): string => {
+    if (theme.mode === "light") {
+      return "city__sky--day";
+    } else {
+      return "city__sky--night";
+    }
+  };
+
   return (
     <div className="city__container">
-      <div className="city__sky"></div>
+      <div className={toggleSky()}></div>
       <img src={SunMoon} alt="" className={"city__" + "sun"} />
       <div className="city__water"></div>
 
@@ -28,10 +37,6 @@ function City() {
         alt=""
         className="city__bridge--bg"
       />
-
-      {/* FOR MOBILE */}
-      {/* <img width="125%" height="auto" src={BridgeForeground} alt="" className="city__bridge--fg" />
-<img width="125%" height="auto" src={BridgeBackground} alt="" className="city__bridge--bg" /> */}
 
       <img
         width="200%"
--- src/Header.tsx
+++ src/Header.tsx
@@ -1,5 +1,6 @@
 import "./Header.css";
 import logo from "./images/Weisberglogo.svg";
+import logoDark from "./images/JW-logo.png";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
 import Switch from "@mui/material/Switch";
@@ -8,9 +9,17 @@ import { useTheme } from "./ThemeManager";
 function Header() {
   const theme = useTheme();
 
+  const logoSwitcher = (): string => {
+    if (theme.mode === "light") {
+      return logo || "";
+    } else {
+      return logoDark || "";
+    }
+  };
+
   return (
     <div className="header__container">
-      <img src={logo} className="header__logo" alt="" />
+      <img src={logoSwitcher()} className="header__logo" alt="" />
       <h1 className="header__textTop"> Front End Developer</h1>
       <div className="header__toggle">
         <LightModeOutlinedIcon className="header__sun" sx={{ fontSize: 40 }} />
