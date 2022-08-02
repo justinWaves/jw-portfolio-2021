604e23c1c172b1cc133ff71a4c704680bd920761
justinWaves <juhstinn@gmail.com>
2022-08-02 00:10:02 -0700

    updated ToolsKnob.tsx component

--- src/Components/ToolsKnob.tsx
+++ src/Components/ToolsKnob.tsx
@@ -23,7 +23,7 @@ function ToolsKnob() {
     if (knobValue === 10) {
       knobSelectHeader = "Javascript ES6";
       knobSelectParagraph =
-        "Javascript is my favorite programming language and the heart of my entire Frontend Stack. As a Front-end focused dev, I have a deep understanding of State and Lifecycle methods in React, event handling, asynchronous code, and strive to deliver the most performant UI components possible";
+        "Javascript is my favorite and most used programming language. As a Front End focused dev, I have a deep understanding of State and Lifecycle methods in React, event handling, asynchronous code, and strive to deliver the most performant UI components possible";
     }
     if (knobValue === 20) {
       knobSelectHeader = "CSS3";
@@ -33,7 +33,7 @@ function ToolsKnob() {
     if (knobValue === 30) {
       knobSelectHeader = "HTML5";
       knobSelectParagraph =
-        "Learned at the same time as CSS in 2014 from CodeAcademy, as well as a strong understanding of the DOM. Today I mainly write JSX in React";
+        "Learned at the same time as CSS in 2013 from CodeAcademy, as well as a strong understanding of the DOM. Today I mainly write JSX in React";
     }
     if (knobValue === 40) {
       knobSelectHeader = "Sass";
@@ -43,7 +43,7 @@ function ToolsKnob() {
     if (knobValue === 50) {
       knobSelectHeader = "Next.JS";
       knobSelectParagraph =
-        "The Next.JS library for React offers some amazing new features I very excited about such as Image optimization, and Environment variables. Deployment in Vercel is also very cool.";
+        "The Next.JS library for React has been a go-to for all my latest projects. Utilizing Image optimization, Server Side Rendering, and deployment to vercel makes for lighting fast UX and better SEO";
     }
     if (knobValue === 60) {
       knobSelectHeader = "Tailwind CSS";
@@ -58,17 +58,17 @@ function ToolsKnob() {
     if (knobValue === 80) {
       knobSelectHeader = "Redux";
       knobSelectParagraph =
-        "I have used Redux, in the past to manage the state of a user. See My LinkedIn Clone below for an example. Initially I planned on using Redux to handle this site's theme toggle state however found Reacts context API to be a much more elegant solution.";
+        "I have used Redux, numerous times to manage global state. See My LinkedIn Clone below for an example. Initially I planned on using Redux to handle this site's theme toggle state however found Reacts context API to be a much more elegant solution.";
     }
     if (knobValue === 90) {
       knobSelectHeader = "React";
       knobSelectParagraph =
-        "React is my favorite front end framework. Major fan of Component-based design. I typically create all React apps purely using functional components, however I can work on a codebase that use Class Components as well.";
+        "React is my favorite and most used front end framework. I eat sleep and breath React apps. ";
     }
     if (knobValue === 100) {
       knobSelectHeader = "Typescript";
       knobSelectParagraph =
-        "Having a perfectly-typed solution makes for much more robust and secure code. This site was implemented in Typescript";
+        "Having a strongly-typed codebase makes for much more robust and secure code. I've grown to really love Typescript despite it yelling at me at times. This site was implemented in Typescript";
     }
   };
 
