92e9189e61aa12dcb87baea0cf52db36e8bc5f5b
Justin Weisberg <juhstinn@gmail.com>
2023-01-12 14:50:18 -0800

    updated bio copy

--- src/Components/BioLeft.tsx
+++ src/Components/BioLeft.tsx
@@ -16,19 +16,13 @@ function BioLeft() {
         <p>
           <span style={{ fontSize: 20 }}>
             {" "}
-            <strong>...and I love to create </strong>
+   
           </span>
-          beautiful and performant web and mobile applications. In 2013, after
-          successfully deploying my first consumer-facing website, I fell in
-          love with the world of software development and dove in deep,
-          completing courses in CS fundamentals, Ruby, PHP, Javascript, HTML &
-          CSS. Two major skills I have developed from being a self-taught
-          programmer, is the ability to learn new tech efficiently, and the
-          ability communicate the needs of many non-technical clients and
-          ultimately ship and deploy final products they are happy with. With a
-          strong foundation in Data Structures, algorithms, Big O, expertise
-          with Typescript, I will ship well tested, production ready code with
-          performance and scalability considerations.
+          ...and I am a Frontend-focused software engineer. From consumer-facing websites, to full-stack web applications, I have been creating digital products since 2013. Today, my stack of choice is React, Next.JS, Typescript, Tailwind, and Redux. 
+With a
+          strong foundation in Data Structures, algorithms, Big O, and expertise
+          with Typescript, I will ship well tested, production ready code optimized for
+          performance and scalability.
           {/* Outside of software development, you can
           find me climbing, playing piano, jamming on guitar, or producing music
           under the name{" "}
