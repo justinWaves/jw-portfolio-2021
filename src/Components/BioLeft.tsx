6b28327095a7b966b0b089b2b9c39407d3c7877d
Justin Weisberg <juhstinn@gmail.com>
2024-03-25 11:22:59 -0700

    updated bio copy

--- src/Components/BioLeft.tsx
+++ src/Components/BioLeft.tsx
@@ -14,13 +14,12 @@ function BioLeft() {
       <img src={bioPic} alt="" />
       <div className="bioLeft__text--contain">
         <p>
-       
-          Composer turned software engineer, I have been transforming ideas into
-          high-performance web applications for the past 10 years. With a strong
-          foundation in Computer Science fundementals and expertise with
-          React, Node.js, and Typescript, I ship well tested, strongly typed,
-          production ready code optimized for performance and scalability.
-          Outside of software development, you can find me climbing, playing
+          Music composer & producer turned software engineer, I have been
+          transforming ideas into high-performance web applications for the past
+          10 years. With a solid foundation in computer science and expertise in
+          React, Node.js, and TypeScript, I deliver well-tested, strongly-typed,
+          production-ready code optimized for performance and scalability.
+          Outside of software development, you can find me climbing, playing the
           piano/guitar, or producing music under the name{" "}
           <BioLink href="https://justinwaves-blog.vercel.app/" target="_blank">
             Justin Waves 🌊
