0ad6ce75c4d3bae5eb2305c7d87d56df0267b433
Justin Weisberg <juhstinn@gmail.com>
2025-06-19 14:27:56 -0700

    edited header copy

--- src/Containers/Header.tsx
+++ src/Containers/Header.tsx
@@ -22,7 +22,7 @@ function Header() {
     <HeaderContainer className="header__container">
       <Logo className="header__logo" fill={isLight ? "#fff" : "#8A8A8A"} />
       <div className="header__text--container">
-        <h1 className="header__textTop"> Software Developer</h1>
+        <h1 className="header__textTop"> Software Engineer</h1>
         <p className="header__subText">San Francisco, CA</p>
       </div>
       <div className="header__toggle--container">
